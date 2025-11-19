38d74a44864640cfe5ec5cbd178bed4b0fa77a7c
Raj <rajkishoreandia@gmail.com>
2025-11-20 01:44:42 +0530

    reduced limit to 50 for API call

--- Reactjs/My-forst-react-app/src/pages/ApiCall.jsx
+++ Reactjs/My-forst-react-app/src/pages/ApiCall.jsx
@@ -8,7 +8,7 @@ function ApiCall() {
     }, []);
 
     const getPhotosGallery = async () => {
-        const res = await fetch('https://picsum.photos/v2/list?page=2&limit=100');
+        const res = await fetch('https://picsum.photos/v2/list?page=2&limit=50');
         const photosDetails = await res.json();
         setphotosList(photosDetails);
     };
@@ -20,7 +20,7 @@ function ApiCall() {
             </h3>
 
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 p-6">
                 {photosList.map((photo) => (
                     <div key={photo.id} className="bg-white rounded shadow p-4 text-center">
                         <img

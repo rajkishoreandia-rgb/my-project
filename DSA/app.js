6dbd90cb5fca3e7dcee6e16f782d653ecdd8a26f
Raj <rajkishoreandia@gmail.com>
2025-11-20 23:46:30 +0530

    started DSA practice

---
+++ DSA/app.js
@@ -0,0 +1,31 @@
+// const a = 10;
+// console.log(a);
+// let prompt = require('prompt-sync')()
+// let name = prompt("Enter your name")
+// console.log(`Your name is ${name}`);
+// Swapping 2 nos using 3 methods
+// method -1
+// let a = 23123;
+// let b = 2424;
+// let temp;
+// temp = a;
+// a=b;
+// b=temp;
+// console.log(a,b);
+// method -2
+let a = 10;
+let b = 20;
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// console.log(a, b);
+// method -3 destructuring
+[a,b]=[b,a];
+console.log(a, b);
+
+
+
+
+
+
+
